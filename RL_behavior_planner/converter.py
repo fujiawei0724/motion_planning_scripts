9ff65a43cdd5d46f678e4465e6b8c9e1b31e70df
fjw <1479028470@qq.com>
2021-12-12 16:49:56 +0800

    Complete converter result test
    E

--- RL_behavior_planner/converter.py
+++ RL_behavior_planner/converter.py
@@ -15,6 +15,11 @@ import torchvision
 from Double_DQN_net import DQN
 
 if __name__ == '__main__':
+    # Official model
+    # model = torchvision.models.resnet18()
+    # traced_script_module = torch.jit.script(model)
+    # traced_script_module.save('model.pt')
+
     model = DQN(94, 231)
     model.load_state_dict(torch.load('./DDQN_weights/checkpoint0.pt', map_location='cpu'))
     model.eval()
@@ -22,6 +27,9 @@ if __name__ == '__main__':
     traced_module = torch.jit.trace(model, dummy_example)
     traced_module.save('model.pt')
 
-    # model = torchvision.models.resnet18()
-    # traced_script_module = torch.jit.script(model)
-    # traced_script_module.save('model.pt')
+    test_input = torch.ones(1, 94)
+    test_res = model.forward(test_input)
+    print('Test res: {}'.format(test_res))
+
+
+
