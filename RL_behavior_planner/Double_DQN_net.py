4c6be18dba7dbcaf97bb9244fff3508cd01bae94
fjw <1479028470@qq.com>
2021-12-24 17:35:20 +0800

    Add a new DQN net with residual block

--- RL_behavior_planner/Double_DQN_net.py
+++ RL_behavior_planner/Double_DQN_net.py
@@ -34,4 +34,34 @@ class DQN(torch.nn.Module):
         x = self.head(x)
         return x
 
+class DQN_resi(torch.nn.Module):
+    def __init__(self, input_num, action_num):
+        super(DQN_resi, self).__init__()
+        # Construct network
+        self.fc1_ = torch.nn.Linear(input_num, 512)
+        self.fc2_ = torch.nn.Linear(512, 512)
+        self.fc3_ = torch.nn.Linear(512, 512)
+        self.fc4_ = torch.nn.Linear(512, 512)
+        self.fc5_ = torch.nn.Linear(512, 512)
+        self.fc6_ = torch.nn.Linear(512, 512)
+        self.fc7_ = torch.nn.Linear(512, 512)
+        self.fc8_ = torch.nn.Linear(512, 512)
+        self.fc9_ = torch.nn.Linear(512, 512)
+        self.head_ = torch.nn.Linear(512, action_num)
+
+    def forward(self, x):
+        x = torch.nn.ReLU()(self.fc1_(x))
+        x = torch.nn.ReLU()(self.fc2_(x))
+        x = torch.nn.ReLU()(self.fc3_(x))
+        x_resi_1 = x.clone()
+        x = torch.nn.ReLU()(self.fc4_(x))
+        x = torch.nn.ReLU()(self.fc5_(x) + x_resi_1)
+        x_resi_2 = x.clone()
+        x = torch.nn.ReLU()(self.fc6_(x))
+        x = torch.nn.ReLU()(self.fc7_(x) + x_resi_2)
+        x_resi_3 = x.clone()
+        x = torch.nn.ReLU()(self.fc8_(x))
+        x = torch.nn.ReLU()(self.fc9_(x) + x_resi_3)
+        x = self.head_(x)
+        return x
 
