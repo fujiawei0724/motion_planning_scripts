cd8b0987c63940907104bc4a1fc8336f9838baea
fjw <1479028470@qq.com>
2022-01-05 15:54:34 +0800

    Add data analysis

--- RL_behavior_planner/converter.py
+++ RL_behavior_planner/converter.py
@@ -20,12 +20,12 @@ if __name__ == '__main__':
     # traced_script_module = torch.jit.script(model)
     # traced_script_module.save('model.pt')
 
-    model = DQN(94, 63)
-    model.load_state_dict(torch.load('./model/checkpoint0.pt', map_location='cpu'))
+    model = DQN(94, 231)
+    model.load_state_dict(torch.load('./model/checkpoint2.pt', map_location='cpu'))
     model.eval()
     dummy_example = torch.rand(1, 94)
     traced_module = torch.jit.trace(model, dummy_example)
-    traced_module.save('./model/model0.pt')
+    traced_module.save('./model/model2.pt')
 
     test_input = torch.ones(1, 94)
     test_res = model.forward(test_input)
---
+++ data_analysis/csvDataAnalysis.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/1/5 下午3:51
+# @Author  : fjw
+# @Email   : fujiawei0724@gmail.com
+# @File    : csv_data_analysis.py
+# @Software: PyCharm
+
+"""
+Grab the data from simulation csv and analyze in multi-extent.
+"""
+
+
