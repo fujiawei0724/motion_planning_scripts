8dbb6ff885051adf8ea2da5f7b24fef12f912e64
fjw <1479028470@qq.com>
2022-05-05 21:02:00 +0800

    Complete image generator

--- RL_behavior_planner/imageGenerator.py
+++ RL_behavior_planner/imageGenerator.py
@@ -2,7 +2,7 @@
 Author: fujiawei0724
 Date: 2022-04-27 17:29:13
 LastEditors: fujiawei0724
-LastEditTime: 2022-04-28 22:23:47
+LastEditTime: 2022-05-05 18:08:12
 Description: Generate the image to represent state.
 '''
 
@@ -20,8 +20,9 @@ class ImageGenerator:
     '''
     description: initialize the size of the image.
     '''    
-    def __init__(self, size=(300, 60, 1), scale=3):
+    def __init__(self, size=(300, 60, 1), lane_width=3.5, scale=3.0):
         self.image_size_ = size
+        self.lane_width_ = lane_width
         self.scale_ = scale
 
     '''
@@ -37,41 +38,61 @@ class ImageGenerator:
         canvas = np.zeros(self.image_size_, dtype='uint8')
 
         # Supple lanes
-        cv2.line(canvas, (round(self.image_size_[1] / 2), 0), (round(self.image_size_[1] / 2), self.image_size_[0]), white, 2)
+        cv2.line(canvas, (round(self.image_size_[1] / 2), 0), (round(self.image_size_[1] / 2), self.image_size_[0]), white, round(self.lane_width_ * self.scale_))
         if lane_info[0] == 1:
-            cv2.line(canvas, (round(self.image_size_[1] / 2 + lane_info[2]), 0), (round(self.image_size_[1] / 2 + lane_info[2]), self.image_size_[0]), white, 2)
+            cv2.line(canvas, (round(self.image_size_[1] / 2 + lane_info[2] * self.scale_ / 2), 0), (round(self.image_size_[1] / 2 + lane_info[2] * self.scale_ / 2), self.image_size_[0]), white, round(self.lane_width_ * self.scale_))
         if lane_info[1] == 1:
-            cv2.line(canvas, (round(self.image_size_[1] / 2 - lane_info[3]), 0), (round(self.image_size_[1] / 2 - lane_info[3]), self.image_size_[0]), white, 2)
+            cv2.line(canvas, (round(self.image_size_[1] / 2 - lane_info[3] * self.scale_ / 2), 0), (round(self.image_size_[1] / 2 - lane_info[3] * self.scale_ / 2), self.image_size_[0]), white, round(self.lane_width_ * self.scale_))
 
         # Supple surround vehicles
         for sur_veh in surround_vehicles_info.values():
-            v_1, v_2 = ImageGenerator.calculateVertice(sur_veh)
-            print('Frenet x: {}, y: {}'.format(sur_veh.position_.x_, sur_veh.position_.y_))
+            # v_1, v_2, v_3, v_4 = self.calculateVertice(sur_veh)
+            print('Frenet x: {}, y: {}, theta: {}'.format(sur_veh.position_.x_, sur_veh.position_.y_, sur_veh.position_.theta_))
             # print('Image x: {}, y: {}'.format(self.positionTransform(v_1), self.positionTransform(v_2)))
-            cv2.rectangle(canvas, self.positionTransform(v_1), self.positionTransform(v_2), black, -1)
+            veh_rotated_rec = self.calculateRotatedRectangleInfo(sur_veh)
+            box = cv2.boxPoints(veh_rotated_rec)
+            box = np.int0(box)
+            cv2.drawContours(canvas, [box], 0, black, -1)
 
         cv2.imshow('Canvas', canvas)
         cv2.waitKey(0)
+
+    '''
+    description: calculate the information for drawing rotated rectangles
+    param {*}
+    return {*}
+    '''   
+    def calculateRotatedRectangleInfo(self, vehicle):
+        # center_position = PathPoint(vehicle.position_.x_ * self.scale_, vehicle.position_.y_ * self.scale_, vehicle.position_.theta_)
+        center_pos = (vehicle.position_.x_ * self.scale_, vehicle.position_.y_ * self.scale_)
+        length = vehicle.length_ * self.scale_
+        width = vehicle.width_ * self.scale_
+        return (self.positionTransform(center_pos), (length, width), -vehicle.position_.theta_ * 180.0 / np.pi + 90)
     
     '''
     description: calculate the rectangle's vertice of a vehicle.
     '''
-    @staticmethod 
-    def calculateVertice(vehicle):
-        center_position = vehicle.position_
-        length = vehicle.length_
-        width = vehicle.width_
-        v_1 = (round(center_position.x_ + length * 0.5 * np.cos(center_position.theta_) - width * 0.5 * np.sin(center_position.theta_)), round(center_position.y_ + length * 0.5 * np.sin(center_position.theta_) + width * 0.5 * np.cos(center_position.theta_)))
-        v_2 = (round(center_position.x_ - length * 0.5 * np.cos(center_position.theta_) + width * 0.5 * np.sin(center_position.theta_)), round(center_position.y_ - length * 0.5 * np.sin(center_position.theta_) - width * 0.5 * np.cos(center_position.theta_)))
-        return v_1, v_2
+    def calculateVertice(self, vehicle):
+        center_position = PathPoint(vehicle.position_.x_ * self.scale_, vehicle.position_.y_ * self.scale_, vehicle.position_.theta_)
+        length = vehicle.length_ * self.scale_
+        width = vehicle.width_ * self.scale_
+        v_1 = (round(center_position.x_ + length * 0.5 * np.cos(center_position.theta_) - width * 0.5 * np.sin(center_position.theta_)), 
+              round(center_position.y_ + length * 0.5 * np.sin(center_position.theta_) + width * 0.5 * np.cos(center_position.theta_)))
+        v_2 = (round(center_position.x_ + length * 0.5 * np.cos(center_position.theta_) + width * 0.5 * np.sin(center_position.theta_)), 
+              round(center_position.y_ + length * 0.5 * np.sin(center_position.theta_) - width * 0.5 * np.cos(center_position.theta_)))
+        v_3 = (round(center_position.x_ - length * 0.5 * np.cos(center_position.theta_) + width * 0.5 * np.sin(center_position.theta_)), 
+              round(center_position.y_ - length * 0.5 * np.sin(center_position.theta_) - width * 0.5 * np.cos(center_position.theta_)))
+        v_4 = (round(center_position.x_ - length * 0.5 * np.cos(center_position.theta_) - width * 0.5 * np.sin(center_position.theta_)), 
+              round(center_position.y_ - length * 0.5 * np.sin(center_position.theta_) + width * 0.5 * np.cos(center_position.theta_)))
+
+        return v_1, v_2, v_3, v_4
     
     '''
     description: transform the position from frenet to image. 
-
     '''    
     def positionTransform(self, frenet_pos):
         # return (self.image_size_[0] - frenet_pos[0], round(self.image_size_[1] / 2) + frenet_pos[1])
-        return (round(self.image_size_[1] / 2) + frenet_pos[1], self.image_size_[0] - frenet_pos[0])
+        return [round(self.image_size_[1] / 2) - frenet_pos[1], self.image_size_[0] - frenet_pos[0]]
 
         
 
@@ -104,8 +125,10 @@ if __name__ == '__main__':
     # cv2.waitKey(0)
 
     # random.seed(0)
-    lane_info = [1, 1, 10, 10]
+    lane_info = [1, 1, 4.0, 4.0]
     agent_generator = AgentGenerator(lane_info[0], lane_info[1], lane_info[2], lane_info[3])
-    surround_vehicles = agent_generator.generateAgents(1)
+    surround_vehicles = agent_generator.generateAgents(10)
     image_generator = ImageGenerator()
+    # cur_vehicle = Vehicle(0, PathPoint(30.0, 0.0, -30.0 / 180.0 * np.pi), 5.0, 2.0, 10.0, 0.0, None, 0.0, 0.0)
+    # test_vehicles = {0: cur_vehicle}
     image_generator.generateSingleImage(lane_info, surround_vehicles)
