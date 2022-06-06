aa7bbd1548ba728678728e3b87c40524bece2b5d
fjw <1479028470@qq.com>
2022-06-06 18:43:49 +0800

    Add single step generator

--- mcts_planner/subEnvironment.py
+++ mcts_planner/subEnvironment.py
@@ -2,13 +2,14 @@
 Author: fujiawei0724
 Date: 2022-05-30 16:54:57
 LastEditors: fujiawei0724
-LastEditTime: 2022-06-01 12:48:49
-Description: Monte Carlo Tree Search algorithm.
+LastEditTime: 2022-06-06 18:43:09
+Description: Components for MCTS.
 '''
 
 import sys
 sys.path.append('..')
 import numpy as np
+import matplotlib.pyplot as plt
 from rl_behavior_planner.utils import *
 from rl_behavior_planner.environment import Environment
 
@@ -57,13 +58,46 @@ class SubForwardExtender(ForwardExtender):
         return next_ego_vehicle, next_sur_vehicles
 
 
+# Evaluate single state generate from subenvironment
+# TODO: if the reward in mcts could only be gotten from the terminal of each episode, this evaluator will be useless
+class StateEvaluator:
+    pass
+
+
 class SubEnvironment(Environment):
 
     def runOnce(self):
         pass
     
-    def simulateSingleStep(self, vehicle_intention):
-        pass
+    def simulateSingleStep(self, vehicle_intention, ax=None):
+        # Initialize state cache
+        # TODO: check this logic, try to avoid the use of copy
+        vehicles = copy.deepcopy(self.surround_vehicle_)
+        vehicles[0] = copy.deepcopy(self.ego_vehicle_)
+
+        # # DEBUG
+        # if ax != None:
+        #     self.visualization(ax)
+        # # END DEBUG
+
+
+        # Calculate next states for each vehicle
+        # TODO: check this parameters about prediction time scan
+        sub_forward_extender = SubForwardExtender(self.lane_server_, 0.4)
+        n_ego_vehicle, n_sur_vehicles = sub_forward_extender.multiAgentForwardOnce(vehicle_intention, 
+                                                                      vehicles, 
+                                                                      self.lane_speed_limit_)
+
+        # # DEBUG
+        # self.ego_vehicle_ = n_ego_vehicle
+        # self.surround_vehicle_ = n_sur_vehicles
+        # if ax != None:
+        #     self.visualization(ax)
+        # # END DEBUG
+
+        return n_ego_vehicle, n_sur_vehicles
+        
+
 
         
 
@@ -71,7 +105,45 @@ class SubEnvironment(Environment):
 
 
 if __name__ == '__main__':
-    pass
+    # Set random seed
+    random.seed(3)
+
+    # Construct environment 
+    env = SubEnvironment()
+
+    # Construct training environment
+    left_lane_exist = random.randint(0, 1)
+    right_lane_exist = random.randint(0, 1)
+    center_left_distance = random.uniform(3.0, 4.5)
+    center_right_distance = random.uniform(3.0, 4.5)
+    lane_info = [left_lane_exist, right_lane_exist, center_left_distance, center_right_distance]
+    lane_speed_limit = random.uniform(10.0, 25.0)
+    lane_info_with_speed = [left_lane_exist, right_lane_exist, center_left_distance, center_right_distance, lane_speed_limit]
+
+    # Construct ego vehicle and surround vehicles randomly
+    ego_vehicle = EgoInfoGenerator.generateOnce()
+    ego_vehicle.curvature_, ego_vehicle.steer_ = 0.0, 0.0
+    ego_vehicle.velocity_ = 2.0
+    ego_vehicle.print()
+    surround_vehicles_generator = AgentGenerator(left_lane_exist, right_lane_exist, center_left_distance, center_right_distance)
+    surround_vehicles = surround_vehicles_generator.generateAgents(random.randint(0, 10))
+
+    # Load data to environment
+    env.load(lane_info_with_speed, ego_vehicle, surround_vehicles)
+
+    # Construct behavior/intention
+    vehicle_intention = VehicleIntention(LateralBehavior.LaneKeeping, -5.0)
+
+    # Calculate next state
+    fig = plt.figure(0)
+    ax = plt.axes()
+    n_ego_vehicle, n_surround_vehicles = env.simulateSingleStep(vehicle_intention, ax)
+    ax.axis('equal')
+    n_ego_vehicle.print()
+
+    plt.show()
+
+
     
 
 
